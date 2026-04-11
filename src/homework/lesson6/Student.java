17980fd82d83ca5626a67a390d56bcd3f28cb2e8
Zaur <zaurik87@gmail.com>
2026-04-11 20:18:19 +0400

    refactor homework6 Student class

--- src/homework/lesson6/Student.java
+++ src/homework/lesson6/Student.java
@@ -35,7 +35,19 @@ public class Student {
 class StudentTest {
     public static void main(String[] args) {
         Student st1 = new Student();
-        Student st2 = new Student(12, "Zaur", "Babayev", 2, 5.5, 6.7, 8.3);
-        Student st3 = new Student(1, "Vusal", "Hesenov", 2);
+        st1.studentId = 2;
+        st1.name = "James";
+        st1.surname = "Bond";
+        st1.course = 3;
+        st1.mathAvgGrade = 4.0;
+        st1.economyAvgGrade = 5.0;
+        st1.foreignLanguageAvgGrade = 6.0;
+
+        Student st2 = new Student(1, "Vusal", "Hesenov", 2);
+        st2.mathAvgGrade = 4.0;
+        st2.economyAvgGrade = 5.0;
+        st2.foreignLanguageAvgGrade = 6.0;
+
+        Student st3 = new Student(12, "Zaur", "Babayev", 2, 5.5, 6.7, 8.3);
     }
 }
