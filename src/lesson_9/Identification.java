9a075cd55601459691fd3a242f6495e2f4e0e58b
Zaur <zaurik87@gmail.com>
2026-04-26 13:39:44 +0400

    identification, variable scope, garbage colector

---
+++ src/lesson_9/Identification.java
@@ -0,0 +1,26 @@
+package lesson_9;
+
+public class Identification {
+    int abbabababababbaaba = 5; // identifikatorun uzunluğunda heç bir məhdudiyyət yoxdur.
+    int €sdsd = 12; // identifikator böyük və ya kiçik hərflərlə, _ və müxtəlif valyuta simvolu ilə başlaya bilər.
+    int A = 34;
+    int $dsjd = 45;
+    int _abc = 17;
+    int a23 = 76; // _ və valyuta identifikatorun istənilən yerində ola bilər, lakin rəqəm öndən başqa hər yerdə ola bilər.
+
+
+    void abc(int a, int b) {
+
+    }
+
+    void abcd(int a, int b) {
+
+    }
+
+
+
+    public static void main(String[] args) {
+        Identification identification = new Identification();
+        System.out.println(identification.€sdsd);
+    }
+}
---
+++ src/lesson_9/Student.java
@@ -0,0 +1,15 @@
+package lesson_9;
+
+public class Student {
+
+}
+
+class StudentTest {
+    public static void main(String[] args) {
+        Student st1 = new Student();
+        Student st2 = new Student();
+        Student st3; // heç bir obyekt yaranmayıb. st3 null-dur.
+        st1 = null; // st1 artıq heç bir obyektə referans deyil. ona bağlı olan obyekt artıq əlçatan deyildir. garbage collector tərəfindən müəyyən vaxtan sonra silinəcəkdir.
+        // scope bitdikdə obyektin həyat dövrü bitir.
+    }
+}
