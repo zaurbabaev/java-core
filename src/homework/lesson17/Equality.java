0f758a49145e4ba277851dc3c3264e03fe993fac
Zaur <zaurik87@gmail.com>
2026-05-13 11:37:26 +0400

    lesson 17 refresh

--- src/homework/lesson17/Equality.java
+++ src/homework/lesson17/Equality.java
@@ -22,6 +22,21 @@ public class Equality {
         return sb1.toString().contentEquals(sb2);
     }
 
+    public static boolean equals3(StringBuilder sb1, StringBuilder sb2) {
+        boolean result = true;
+        if (sb1.length() == sb2.length()) {
+            for (int i = 0; i < sb1.length(); i++) {
+                if (sb1.charAt(i) != sb2.charAt(i)) {
+                    result = false;
+                    break;
+                }
+            }
+        } else {
+            result = false;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
 
         StringBuilder sb1 = new StringBuilder("Hello");
